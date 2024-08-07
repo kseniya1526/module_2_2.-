d7b2b5e1f4767dbe8da70b667475ee82f8e188fd
kseniya1526 <kseniak505@gmail.com>
2024-08-07 20:28:48 +0700

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,19 @@
+first = 123
+second = 456
+third = 789
+if first == second == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
+    
+first_ = 42
+second_ = 69
+third_ = 42
+if first_ == second_ == third_:
+    print(3)
+elif first_ == second_ or first_ == third_ or second_ == third_:
+    print(2)
+else:
+    print(0)
